37cbb069dcd86f8daa9b94d0c903c765a5bafbff
Ezequiel Silveira <ezequiel.l.silveira@gmail.com>
2025-01-24 00:12:22 -0300

    formatter, http client; reactive form

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.css'
+  styleUrl: './welcome.component.css',
 })
 export class WelcomeComponent {
-  loginForm = {
-    nickname: '',
-    password: '',
-  }
+  constructor(private http: HttpClient) {}
+  readonly loginForm = new FormGroup({
+    nickname: new FormControl(''),
+    password: new FormControl(''),
+  });
 
-  submit(event: Event): String {
-    event.preventDefault();
-    console.log('loginForm', this.loginForm)
-    return '';
+  submit(): void {
+    // event.preventDefault();
+    const baseUrl: string = 'http://localhost:3000/';
+    console.log('this.loginForm', this.loginForm.value);
+    // console.log('loginForm', loginForm)
+    this.http.post(`${baseUrl}auth/login`, this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('response', response);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
   }
+  // curl -X POST http://localhost:3000/auth/login -d '{"username": "ezeqi", "password": "password"}' -H "Content-Type: application/json"
 }
